ff6c65f6f4f6a77803eae40d3dc7943f54127d77
Jacob MacFarland <macfarlandjw@appstate.edu>
2020-12-25 13:13:52 -0600

    Initial Commit

---
+++ SwiftUIAppPractice/SwiftUIAppPracticeApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIAppPracticeApp.swift
+//  SwiftUIAppPractice
+//
+//  Created by Jacob MacFarland on 12/25/20.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIAppPracticeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
